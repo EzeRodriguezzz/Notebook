140e3ca39962dc9e9f452f2a310c19936c4b6169
Ezequiel Rodriguez <ezequiel.rodriguez@mudafy.com>
2022-04-18 13:25:59 -0300

    agregar constantes y metodo necesario para el flow

--- flow.py
+++ flow.py
@@ -1,6 +1,6 @@
 ########################################## INICIO Confirmar visita 48 hs antes MX ####################################################
 
-from flow_modelo import CONFIRM_BOOKING_4_HOURS_BEFORE, CONFIRM_BOOKING_LESS_24_HOURS, SEND_CONFIRMATION_WHATSAPP, SEND_CONFIRMATION_WHATSAPP_WHEN_BOOKED, get_create_task_callback
+from flow_modelo import CONFIRM_BOOKING_4_HOURS_BEFORE, CONFIRM_BOOKING_LESS_24_HOURS, COORDINATE_VISIT, COORDINATE_VISIT_BOOKER, NOTIFY_ALLIANCE_WHATSAPP, SEND_CONFIRMATION_WHATSAPP, SEND_CONFIRMATION_WHATSAPP_WHEN_BOOKED, create_action_state, create_task_state, get_close_case_callback, get_create_task_callback
 
 
 confirm_booking_48_hours_before = create_task_flow(name="Confirmar visita 48 hs antes", subtype=CONFIRM_BOOKING_48_HOURS_BEFORE, countries=["MX"])
@@ -143,14 +143,167 @@ state_visit_canceled = create_option_state(
     )
 )
 
-# State Canceló visita
+# State Cancelar visita
 state_cancel_visit = create_option_state(
-    key="visit_canceled",
-    name="Canceló visita",
+    key="cancel_visit",
+    name="Cancelar visita",
     flow=confirm_booking_48_hours_before,
     metadata=get_metadata(
         title="¿Por qué se cancela la visita?"
     )
+)
+
+# State Cancelar visita
+state_coordinate_visit_AC_MX = create_task_state(
+    key=COORDINATE_VISIT_BOOKER,
+    name="Coordinar visita AC MX",
+    flow=confirm_booking_48_hours_before,
+    metadata=get_metadata(),
+    is_state_final=True
+)
+
+# State Cancelar visita
+state_notify_alliance_whatsapp = create_task_state(
+    key=NOTIFY_ALLIANCE_WHATSAPP,
+    name="Notificar a Alianza por whatsapp",
+    flow=confirm_booking_48_hours_before,
+    metadata=get_metadata(
+        title="¿Se envió un whatsapp?"
+    )
+)
+
+# State No se envió
+state_notification_not_sent = create_option_state(
+    key="notification_not_sent",
+    name="No se envió",
+    flow=confirm_booking_48_hours_before,
+    metadata=get_metadata()
+)
+
+# State Se envió
+state_notification_sent = create_option_state(
+    key="state_notification_sent",
+    name="Se envió",
+    flow=confirm_booking_48_hours_before,
+    metadata=get_metadata()
+)
+
+# State colocar rol asignado
+state_place_role = create_action_state(
+    key="place_role",
+    name="Colocar rol asignado",
+    flow=confirm_booking_48_hours_before,
+    metadata={
+
+    }
+)
+
+# State colocar rol asignado
+state_cancelation_motive = create_action_state(
+    key="cancelation_motive",
+    name="Motivo de cancelación",
+    flow=confirm_booking_48_hours_before,
+    metadata={
+        
+    }
+)
+
+#State Cerrar caso
+state_close_case = create_task_state (
+    key=CLOSE_CASE,
+    name="Cerrar caso",
+    flow=confirm_booking_48_hours_before,
+    metadata=get_metadata(),
+    is_final_state=True
+)
+
+# Edge entre Contestó y Canceló visita
+edge_13 = Edge.objects.create(
+    from_state=state_replied,
+    to_state=state_visit_canceled,
+    flow=confirm_booking_48_hours_before,
+    callbacks=[],
+    order=2,
+)
+
+# Edge entre Notificar a Alianza por WA y No se envió WA
+edge_14 = Edge.objects.create(
+    from_state=state_notify_alliance_whatsapp,
+    to_state=state_notification_not_sent,
+    flow=confirm_booking_48_hours_before,
+    callbacks=[],
+    order=1,
+)
+
+# Edge entre Notificar a Alianza por WA y Se envió WA
+edge_15 = Edge.objects.create(
+    from_state=state_notify_alliance_whatsapp,
+    to_state=state_notification_sent,
+    flow=confirm_booking_48_hours_before,
+    callbacks=[],
+    order=2,
+)
+
+# Edge de vuelta entre No se envió WA y Notificar a Alianza por WA
+edge_16 = Edge.objects.create(
+    from_state=state_notification_not_sent,
+    to_state=state_notify_alliance_whatsapp,
+    flow=confirm_booking_48_hours_before,
+    callbacks=[
+        get_create_task_callback(kind=NOTIFY_ALLIANCE_WHATSAPP, sla={"minutes": 30}, title="Notificar a Alianza por whatsapp")
+    ],
+    order=1,
+)
+
+# Edge entre Se envió WA y Cerrar caso
+edge_17 = Edge.objects.create(
+    from_state=state_notification_sent,
+    to_state=state_close_case,
+    flow=confirm_booking_48_hours_before,
+    callbacks=[
+        get_close_case_callback()
+    ],
+    order=1,
+)
+
+# Edge entre Canceló visita y Colocal rol asignado
+edge_18 = Edge.objects.create(
+    from_state=state_visit_canceled,
+    to_state=state_place_role,
+    flow=confirm_booking_48_hours_before,
+    callbacks=[],
+    order=2,
+)
+
+# Edge entre Colocal rol asignado y Coordinar visita AC MX
+edge_19 = Edge.objects.create(
+    from_state=state_place_role,
+    to_state=state_coordinate_visit_AC_MX,
+    flow=confirm_booking_48_hours_before,
+    callbacks=[
+        get_create_task_callback(kind=COORDINATE_VISIT, sla={"hours": 2}, title="Coordinar visita AC MX")
+    ],
+    order=1,
+)
+
+# Edge entre Cancelar visita y Colocal motivo de cancelación
+edge_20 = Edge.objects.create(
+    from_state=state_cancel_visit,
+    to_state=state_cancelation_motive,
+    flow=confirm_booking_48_hours_before,
+    callbacks=[],
+    order=1,
+)
+
+# Edge entre Cancelar visita y Colocal motivo de cancelación
+edge_20 = Edge.objects.create(
+    from_state=state_cancelation_motive,
+    to_state=state_notify_alliance_whatsapp,
+    flow=confirm_booking_48_hours_before,
+    callbacks=[
+        get_create_task_callback(kind=NOTIFY_ALLIANCE_WHATSAPP, sla={"minutes": 30}, title="Notificar a Alianza por whatsapp")
+    ],
+    order=1,
 )
 
 
